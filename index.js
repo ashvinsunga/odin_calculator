edcae918488aee2dc9c4930f8c366ce5421e0e5b
Ashvin <155508505+ashvinsunga@users.noreply.github.com>
2024-12-01 12:37:36 +0800

    Buttons and display are now working!

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
+buttons = document.querySelectorAll(".key")
+screen = document.querySelector("#screen")
+let screenValue = "";
 let input1 = 0;
 let input2 = 0;
+
+buttons.forEach(element => {
+    element.addEventListener("click", function () {
+        if (this.classList.contains("operator-key")){
+            setNum()
+            screen.textContent += this.innerHTML
+        } else if (this.classList.contains("clear-key")){
+            clearScreen()
+        } else {
+            populate(this)
+        }
+    })
+});
+
+function populate(element){
+    screenValue += element.dataset.calcValue
+    screen.textContent = screenValue
+}
+
+function anOperator() {
+    console.log("I am an operator!")
+}
+
+
+function clearScreen() {
+    screenValue = ""
+    screen.textContent = screenValue
+}
 
 function add(){
 
@@ -15,6 +46,11 @@ function multiply(){
 
 function divide(){
     
+}
+
+function setNum(){
+    screenValue = parseInt(screenValue)
+    console.log(typeof(screenValue))
 }
 
 function operate(operator, num1, num2){
