2d51d277e56da5cd9452956c579ecc44dd324ce3
Ashvin <155508505+ashvinsunga@users.noreply.github.com>
2024-12-01 10:20:54 +0800

    UI created

---
+++ index.js
@@ -0,0 +1,22 @@
+let input1 = 0;
+let input2 = 0;
+
+function add(){
+
+}
+
+function subtract(){
+    
+}
+
+function multiply(){
+    
+}
+
+function divide(){
+    
+}
+
+function operate(operator, num1, num2){
+
+}
