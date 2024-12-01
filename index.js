c87c76ee98307ba6a05c66b3f9d1123ec41b0ec5
Ashvin <155508505+ashvinsunga@users.noreply.github.com>
2024-12-01 21:01:24 +0800

    Accepting pairs of value and using the result as new input

--- index.js
+++ index.js
@@ -1,58 +1,119 @@
 buttons = document.querySelectorAll(".key")
 screen = document.querySelector("#screen")
 let screenValue = "";
+let value = "";
 let input1 = 0;
 let input2 = 0;
+let calcOperator = ""
 
 buttons.forEach(element => {
     element.addEventListener("click", function () {
         if (this.classList.contains("operator-key")){
-            setNum()
             screen.textContent += this.innerHTML
-        } else if (this.classList.contains("clear-key")){
-            clearScreen()
+            setOperator(this)
+        } else if (this.id === "key-equal") {
+            setSecondNum()
+            operate(calcOperator, input1, input2)
+        }           
+        else if (this.classList.contains("clear-key")){
+            clearValues()
         } else {
             populate(this)
         }
+        
     })
 });
 
 function populate(element){
+    value += element.dataset.calcValue
+    screenValue = screen.textContent
     screenValue += element.dataset.calcValue
+    // console.log(screenValue,calcOperator)
     screen.textContent = screenValue
 }
 
-function anOperator() {
-    console.log("I am an operator!")
+function setFirstNum(){
+    input1 = parseInt(value)
+    value = 0;
+    console.log("setFirstNum was ran", input1)
 }
 
+function setSecondNum(){
+    input2 = parseInt(value)
+    value = 0;
+    console.log("setSecondNum was ran", input2, input1)
+}
+
+function setOperator(element) {
+
+    if (input1 === 0){
+        setFirstNum()
+    } else {
+        setSecondNum()
+    }
+
+
+    if (element.textContent === "x"){
+        calcOperator = "multiplication"
+    }
+
+    if (element.textContent === "%"){
+        calcOperator = "division"
+    }
+
+    if (element.textContent === "+"){
+        calcOperator = "addition"
+    }
+
+    if (element.textContent === "-"){
+        calcOperator = "subtraction"
+    }
+
+}
+
+
+function clearValues() {
+    input1 = 0;
+    input2 = 0;
+    value = 0;
+    calcOperator = "";
+    clearScreen()
+}
 
 function clearScreen() {
-    screenValue = ""
-    screen.textContent = screenValue
+    screenValue = "";
+    screen.textContent = screenValue;
 }
 
-function add(){
-
-}
-
-function subtract(){
-    
-}
-
-function multiply(){
-    
-}
-
-function divide(){
-    
-}
-
-function setNum(){
-    screenValue = parseInt(screenValue)
-    console.log(typeof(screenValue))
-}
 
 function operate(operator, num1, num2){
+// Operation functions
+
+const add = (val1, val2) => val1 + val2
+const subtract = (val1, val2) => val1 - val2
+const divide = (val1, val2) => val1 / val2
+const multiply = (val1, val2) => val1 * val2
+
+let result = 0;
+
+    if (operator === "multiplication"){
+        value = multiply(num1, num2)
+    }
+
+    if (operator === "division"){
+        value = divide(num1, num2)
+    }
+
+    if (operator === "addition"){
+        value = add(num1, num2)
+    }
+
+    if (operator === "subtraction"){
+        value = subtract(num1, num2)
+    }
+    
+    screen.textContent = value
+    input1 = value
+    value = 0;
 
 }
